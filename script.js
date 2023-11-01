5c8a5bd0d75b6d6340ecb1c27e7f16f77cce73f6
Zanjoee <141194049+Zanjoee@users.noreply.github.com>
2023-11-01 17:18:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove('active-link');
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove('active-tab');
+  }
+  event.currentTarget.classList.add('active-link');
+  document.getElementById(tabname).classList.add('active-tab');
+}
+
+var menu = document.getElementById('menu');
+
+function closemenu() {
+  menu.style.right = "-200px";
+}
+
+function openmenu() {
+  menu.style.right = "0"
+}
